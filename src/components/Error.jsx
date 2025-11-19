311559c8bf9dccb50ea4e4f90e81fc2f594bb578
fariha habib <farihahabib2202@gmail.com>
2025-11-19 20:26:33 +0600

    add

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Error = () => {
     return (
-        <div className='flex flex-col justify-center items-center h-screen text-6xl font-bold text-red-600'>
+        <div className='flex flex-col justify-center items-center h-screen text-6xl font-bold text-teal-600'>
             <h1>404</h1>
-            <p>Page not found</p>
+            <p>What Page you search is not found</p>
         </div>
     );
 };
